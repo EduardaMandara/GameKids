1b5289698fa090d04acf43e1367b9ac38888c892
Eduarda Bezerra <eduardaf.bezerra@outlook.com>
2022-04-04 16:01:52 -0300

    trabalhando os comandos em JS

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,3 +1,16 @@
-function somDeBeluga () {
-    document.querySelector('#som__beluga').play();
+function somDeAnimal(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla'); // Referencia constante da lista de teclas dos animais. 
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    listaDeTeclas[contador].onclick = somDeAnimal; // Som do animal.
+
+    contador = contador + 1; //Contador (0) = Contador + 1 | Irá somar o contador com valor anterior, mais 1, até esta codição se tornar falsa, ou seja, até o contador não ser mais < 16.
+
+    console.log(contador);
 }
