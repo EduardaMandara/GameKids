551bdad7fb941a1dbd7600388dd9c0d28164344a
Eduarda Bezerra <eduardaf.bezerra@outlook.com>
2022-04-05 21:08:41 -0300

    Audios completos, Dimensionamento da pagina Ok Main JS Ok

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -2,15 +2,24 @@ function somDeAnimal(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla'); // Referencia constante da lista de teclas dos animais. 
-
 let contador = 0;
+
+const listaDeTeclas = document.querySelectorAll('.tecla'); // Referencia constante da lista de teclas dos animais. 
 
 while (contador < listaDeTeclas.length) {
 
-    listaDeTeclas[contador].onclick = somDeAnimal; // Som do animal.
+    const tecla = listaDeTeclas[contador];
+    const animal = tecla.classList[1];
+
+    const idAudio = `#som__${animal}`; //Template String
+
+    //console.log(idAudio, tecla)
+
+    tecla.onclick = function () {
+        somDeAnimal(idAudio);
+    }
 
     contador = contador + 1; //Contador (0) = Contador + 1 | Irá somar o contador com valor anterior, mais 1, até esta codição se tornar falsa, ou seja, até o contador não ser mais < 16.
 
-    console.log(contador);
+    //console.log(contador);
 }
